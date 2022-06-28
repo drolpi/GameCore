b706456b5439a903c10d2583c8c73459ab6e3078
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2022-06-28 23:42:20 +0200

    Add cancel logic in EventListenerImpl

--- src/main/java/net/bote/gamecore/api/eventbus/EventListenerImpl.java
+++ src/main/java/net/bote/gamecore/api/eventbus/EventListenerImpl.java
@@ -26,6 +26,10 @@ final class EventListenerImpl<T> implements EventListener<T> {
 
     @Override
     public void handle(@NotNull T event) {
+        if (event instanceof CancellableEvent && ((CancellableEvent) event).isCancelled()) {
+            return;
+        }
+
         if (!this.conditions.isEmpty()) {
             for (Predicate<T> condition : this.conditions) {
                 if (!condition.test(event))
