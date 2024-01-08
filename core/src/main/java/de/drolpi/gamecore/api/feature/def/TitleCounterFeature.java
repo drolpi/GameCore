612a24812c92362af6f1c32bcbef94239e83aade
gabl <gabl22ytmc@gmail.com>
2024-01-08 21:41:24 +0100

    fix units in title

--- core/src/main/java/de/drolpi/gamecore/api/feature/def/TitleCounterFeature.java
+++ core/src/main/java/de/drolpi/gamecore/api/feature/def/TitleCounterFeature.java
@@ -6,6 +6,7 @@ import de.drolpi.gamecore.api.game.Game;
 import de.drolpi.gamecore.api.phase.Phase;
 import de.drolpi.gamecore.api.player.GamePlayer;
 import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.minimessage.tag.resolver.Formatter;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.title.Title;
@@ -27,9 +28,10 @@ public class TitleCounterFeature extends AbstractKeyCounterHandlerFeature {
             Component.translatable(preKey + "_subtitle")
         );
         final TagResolver.Single placeholder = Placeholder.component("count", Component.text(counter.currentCount()));
+        final TagResolver secondsFormatter = Formatter.choice("unit_seconds", counter.currentCount());
 
         for (final GamePlayer allPlayer : this.game.allPlayers()) {
-            allPlayer.showTitle(title, placeholder);
+            allPlayer.showTitle(title, placeholder, secondsFormatter);
         }
     }
 }
