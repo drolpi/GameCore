e4209792588010947e71f6923e518064c7e869b8
dasdrolpi <74879793+dasdrolpi@users.noreply.github.com>
2024-01-08 21:30:34 +0100

    feat: add possibility to configure counter handler

--- core/src/main/java/de/drolpi/gamecore/api/feature/def/TitleCounterFeature.java
+++ core/src/main/java/de/drolpi/gamecore/api/feature/def/TitleCounterFeature.java
@@ -1,6 +1,5 @@
 package de.drolpi.gamecore.api.feature.def;
 
-import com.google.gson.annotations.Expose;
 import com.google.inject.Inject;
 import de.drolpi.gamecore.api.counter.Counter;
 import de.drolpi.gamecore.api.game.Game;
@@ -8,56 +7,29 @@ import de.drolpi.gamecore.api.phase.Phase;
 import de.drolpi.gamecore.api.player.GamePlayer;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder;
+import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver;
 import net.kyori.adventure.title.Title;
 
-public class TitleCounterFeature extends AbstractCounterHandlerFeature {
+public class TitleCounterFeature extends AbstractKeyCounterHandlerFeature {
 
     private final Game game;
-    private final Phase phase;
-
-    @Expose
-    private int[] ticks = new int[]{60, 50, 40, 30, 20, 15, 10, 5, 4, 3, 2, 1};
 
     @Inject
-    public TitleCounterFeature(Phase phase, Game game) {
+    public TitleCounterFeature(Game game, Phase phase) {
         super(phase);
         this.game = game;
-        this.phase = phase;
     }
 
     @Override
-    protected void start(Counter counter) {
+    protected void perform(Counter counter, String preKey) {
+        final Title title = Title.title(
+            Component.translatable(preKey + "_title"),
+            Component.translatable(preKey + "_subtitle")
+        );
+        final TagResolver.Single placeholder = Placeholder.component("count", Component.text(counter.currentCount()));
 
-    }
-
-    @Override
-    protected void tick(Counter counter) {
         for (final GamePlayer allPlayer : this.game.allPlayers()) {
-            for (int tick : this.ticks) {
-                if (tick != counter.currentCount()) {
-                    continue;
-                }
-
-                allPlayer.showTitle(
-                    Title.title(Component.translatable(this.phase.key() + ".counter_title"), Component.translatable(this.phase.key() + ".counter_subtitle")),
-                    Placeholder.component("count", Component.text(counter.currentCount()))
-                );
-            }
-        }
-    }
-
-    @Override
-    protected void cancel(Counter counter) {
-
-    }
-
-    @Override
-    protected void finish(Counter counter) {
-        for (final GamePlayer allPlayer : this.game.allPlayers()) {
-            allPlayer.showTitle(
-                Title.title(Component.translatable(this.phase.key() + ".counter_title_finish"), Component.translatable(this.phase.key() + ".counter_subtitle_finish")),
-                Placeholder.component("count", Component.text(counter.currentCount()))
-            );
+            allPlayer.showTitle(title, placeholder);
         }
     }
 }
